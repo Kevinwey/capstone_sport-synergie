f9ddc6a3ee7de1daee87948beac38daf58bae9a3
Kevin Weyrowitz <kevinweyrowitz93@gmail.com>
2023-04-03 15:47:22 +0200

    add random function

--- components/SportList/index.js
+++ components/SportList/index.js
@@ -1,12 +1,25 @@
-import React from "react";
-import sortedSportsData from "../../lib/sports";
+import React, { useState, useEffect } from "react";
+import sportsData from "../../lib/sports";
 
 export default function SportList() {
+  const [sports, setSports] = useState([]);
+
+  useEffect(() => {
+    setSports(getRandomSports());
+  }, []);
+
+  function getRandomSports() {
+    const randomSports = sportsData.sort(() => Math.random() - 0.5).slice(0, 3);
+    return randomSports;
+  }
+
+  sports.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <div>
       <h1>Sports</h1>
       <ul>
-        {sortedSportsData.map((sport) => (
+        {sports.map((sport) => (
           <li key={sport.id}>{sport.name}</li>
         ))}
       </ul>
