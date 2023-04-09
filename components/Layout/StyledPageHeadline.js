9f3cf37dd1441e17a6f48be46f503ef6ccd7e991
Kevin Weyrowitz <kevinweyrowitz93@gmail.com>
2023-04-09 14:03:28 +0200

    add description

--- components/Layout/StyledPageHeadline.js
+++ components/Layout/StyledPageHeadline.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 const StyledPageHeadline = styled.h2`
   color: lightgrey;
   text-align: center;
-  font-size: 2rem;
+  font-size: 2.5rem;
+  margin: 10px;
 `;
 
 export default StyledPageHeadline;
--- components/SportList/index.js
+++ components/SportList/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import sportsData from "../../lib/sports";
 import StyledPageHeadline from "../Layout/StyledPageHeadline";
-import StyledSportsList from "./StyledSportsList";
+import styled from "styled-components";
 import Link from "next/link";
 import StyledButton from "../Layout/StyledButton";
 
 export default function SportList() {
   const [sports, setSports] = useState([]);
+  const [selectedSport, setSelectedSport] = useState(null);
 
   useEffect(() => {
     setSports(getRandomSports());
@@ -22,17 +23,60 @@ export default function SportList() {
 
   sports.sort((a, b) => a.name.localeCompare(b.name));
 
+  function handleSelectSport(sport) {
+    setSelectedSport(sport);
+  }
+
   return (
-    <div>
+    <StyledContainer>
       <StyledPageHeadline>Sports</StyledPageHeadline>
-      <StyledSportsList>
-        {sports.map((sport) => (
-          <li key={sport.id}>{sport.name}</li>
-        ))}
-        <Link href={"/ProfilePage"}>
-          <StyledButton>MyProfile</StyledButton>
-        </Link>
-      </StyledSportsList>
-    </div>
+
+      {sports.map((sport) => (
+        <StyledSelectButton
+          key={sport.id}
+          selected={sport === selectedSport}
+          onClick={() => handleSelectSport(sport)}
+        >
+          {sport.name}
+        </StyledSelectButton>
+      ))}
+      {selectedSport && (
+        <StyledDescription>{selectedSport.description}</StyledDescription>
+      )}
+
+      <Link href={"/ProfilePage"}>
+        <StyledButton>MyProfile</StyledButton>
+      </Link>
+    </StyledContainer>
   );
 }
+
+const StyledSelectButton = styled.button`
+  background-color: ${(props) => (props.selected ? "grey" : "white")};
+  color: ${(props) => (props.selected ? "blue" : "black")};
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  font-size: 1.2rem;
+  width: 200px;
+  cursor: pointer;
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledDescription = styled.p`
+  font-size: 1.2rem;
+  margin: 10px;
+  text-align: center;
+  width: 300px;
+  color: grey;
+  height: 180px;
+  border: 1px dotted black;
+  border-radius: 10px;
+`;
