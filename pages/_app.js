624ccc807d21ae148ef5bf6c24b0925798fa5180
Kevin Weyrowitz <kevinweyrowitz93@gmail.com>
2023-04-09 10:42:01 +0200

    refactor states

--- pages/_app.js
+++ pages/_app.js
@@ -3,62 +3,52 @@ import Layout from "../components/Layout";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
-  const [age, setAge] = useState("");
-  const [height, setHeight] = useState("");
-  const [weight, setWeight] = useState("");
-  const [intensity, setIntensity] = useState("");
-  const [category, setCategory] = useState("All");
-
-  const [physique, setPhysique] = useState([
-    { name: "Slim", checked: false },
-    { name: "Normal", checked: false },
-    { name: "Strong", checked: false },
-  ]);
-  const [fitnessLevel, setFitnessLevel] = useState([
-    { name: "Unatheltic", checked: false },
-    { name: "Fit", checked: false },
-    { name: "Muscular", checked: false },
-  ]);
-  const [timePerWeek, setTimePerWeek] = useState([
-    { name: "2-3x", checked: false },
-    { name: "3-4x", checked: false },
-    { name: "4+", checked: false },
-  ]);
-  const [preference, setPreference] = useState([
-    { name: "Teamsport", checked: false },
-    { name: "Individual sports", checked: false },
-    { name: "Indoor", checked: false },
-    { name: "Outdoor", checked: false },
-  ]);
-
-  const inputFields = {
-    age: setAge,
-    height: setHeight,
-    weight: setWeight,
-    intensity: setIntensity,
-    category: setCategory,
-  };
+  const [formData, setFormData] = useState({
+    age: "",
+    height: "",
+    weight: "",
+    intensity: "",
+    category: "",
+    physique: [
+      { name: "Slim", checked: false },
+      { name: "Normal", checked: false },
+      { name: "Strong", checked: false },
+    ],
+    fitnessLevel: [
+      { name: "Unathletic", checked: false },
+      { name: "Fit", checked: false },
+      { name: "Muscular", checked: false },
+    ],
+    timePerWeek: [
+      { name: "2-3x", checked: false },
+      { name: "3-4x", checked: false },
+      { name: "4+", checked: false },
+    ],
+    preference: [
+      { name: "Teamsport", checked: false },
+      { name: "Individual sports", checked: false },
+      { name: "Indoor", checked: false },
+      { name: "Outdoor", checked: false },
+    ],
+  });
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    inputFields[name](value);
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
   }
 
   function handleChange(type, name) {
-    const stateVariables = {
-      physique: [physique, setPhysique],
-      fitnessLevel: [fitnessLevel, setFitnessLevel],
-      timePerWeek: [timePerWeek, setTimePerWeek],
-      preference: [preference, setPreference],
-    };
+    setFormData((prevState) => {
+      const newState =
+        type === "preference"
+          ? updatePreference(prevState[type], name)
+          : updateSingleItem(prevState[type], name);
 
-    const [state, setState] = stateVariables[type];
-    const newState =
-      type === "preference"
-        ? updatePreference(state, name)
-        : updateSingleItem(state, name);
-
-    setState(newState);
+      return {
+        ...prevState,
+        [type]: newState,
+      };
+    });
   }
 
   function updateSingleItem(state, name) {
@@ -81,17 +71,7 @@ export default function App({ Component, pageProps }) {
       <Layout />
       <Component
         {...pageProps}
-        formData={{
-          age,
-          height,
-          weight,
-          intensity,
-          category,
-          physique,
-          fitnessLevel,
-          timePerWeek,
-          preference,
-        }}
+        formData={formData}
         onChange={handleInputChange}
         handleChange={handleChange}
       />
